9749aa17e07d1e88d18fab337b42a5f57706dd2e
Hydro844 <hydro844@cock.li>
2016-09-12 19:41:51 +0200

    everything in one file c:

--- Core.lua
+++ Core.lua
@@ -1,9 +1,29 @@
+print("achievement lua starts")
+
+local Achievement = {}
+--Achievement.__index = Achievement
+
+function Achievement:new()
+    print("in achievement methode new in")
+    setmetatable({}, Achievement)
+    print("proceeded in achievement new method")
+    return self
+end
+
+function Achievement:hello()
+    print("Hello World!")
+end
+
+print("achievement lua ends")
+
+----------------------------------------------
+
 print("core lua starts")
 
-local achievement = Achievement:new()
+local a = Achievement:new()
 
 print("core in the middle")
 
-achievement:hello()
+a:hello()
 
 print("core lua ends")
